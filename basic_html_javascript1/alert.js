0d374767c06ae49d56e01649f6fae44d84490cab
eaevanarnett <85958448+eaevanarnett@users.noreply.github.com>
2021-06-22 11:04:18 -0700

    Performed all assignments on the 'assignment.html' because of the content already provided

---
+++ basic_html_javascript1/alert.js
@@ -0,0 +1,7 @@
+alert("Our website makes use of 'cookies' to personalize user experience. Click OK to continue.");
+function my_first_function() {
+				var string = "Making Progress!";
+				var result = string.fontcolor("teal");
+				document.getElementById("thought").innerHTML = 
+				result;
+			}
